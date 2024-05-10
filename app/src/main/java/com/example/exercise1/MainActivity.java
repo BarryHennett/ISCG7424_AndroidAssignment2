1e744d00f869d7dfa3d179646606893a2a110744
Harry Bennett <harrybennettnz@gmail.com>
2024-05-10 14:02:07 +1200

    linked the firebase

--- app/src/main/java/com/example/exercise1/MainActivity.java
+++ app/src/main/java/com/example/exercise1/MainActivity.java
@@ -9,6 +9,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,5 +21,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
